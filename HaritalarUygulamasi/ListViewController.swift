b272e792fd2e73a7002dd9c0576bbd379464d361
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-03 22:55:01 +0300

    mapsVC ve mainVC eklendi. Lefttopbar add button eklendi.

---
+++ HaritalarUygulamasi/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewController.swift
+//  HaritalarUygulamasi
+//
+//  Created by İbrahim Halid Bayrak on 3.08.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(artiButtonuiklandi))
+    }
+    
+    @objc func artiButtonuiklandi() {
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+}
