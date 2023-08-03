fce37f7e317faebb007151ca040bcc65166f4a1e
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-03 22:43:43 +0300

    core data entegre edildi.

--- HaritalarUygulamasi/ViewController.swift
+++ HaritalarUygulamasi/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-    var locationManager = CLLocationManager()
     
     
+    @IBOutlet weak var isimTextField: UITextField!
+    @IBOutlet weak var notTextField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var secilenLatitude = Double()
+    var secilenLongitude = Double()
     
     
     
@@ -38,10 +44,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             let dokunulanNokta = gestureRecognizer.location(in: mapView)
             let dokonulanKordinat = mapView.convert(dokunulanNokta, toCoordinateFrom: mapView)
             
+            secilenLatitude = dokonulanKordinat.latitude
+            secilenLongitude = dokonulanKordinat.longitude
+            
             let annotation = MKPointAnnotation()
             annotation.coordinate = dokonulanKordinat
-            annotation.title = "Kullanıcı Seçimi"
-            annotation.subtitle = "Altyazı"
+            annotation.title = isimTextField.text
+            annotation.subtitle = notTextField.text
             mapView.addAnnotation(annotation)
             
         }
@@ -57,6 +66,26 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.setRegion(region, animated: true)
     }
 
-
+    @IBAction func kaydetTiklandi(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let yeniYer = NSEntityDescription.insertNewObject(forEntityName: "Yer", into: context)
+        
+        yeniYer.setValue(isimTextField.text, forKey: "isim")
+        yeniYer.setValue(notTextField.text, forKey: "not")
+        yeniYer.setValue(secilenLatitude, forKey: "latitude")
+        yeniYer.setValue(secilenLongitude, forKey: "longitude")
+        yeniYer.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            print("Kayıt Edildi!")
+        } catch {
+            print("Hata var!")
+        }
+    }
+    
 }
 
